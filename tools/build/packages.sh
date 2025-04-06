bc0c0d0d7af9487fb9e55c5dfc7b84e64c70b394
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-06 19:23:17 -0400

    Update packages.sh

--- tools/build/packages.sh
+++ tools/build/packages.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
 sudo apt-get update
+sudo apt update
+sudo apt install python2
+sudo apt install python-is-python2
+sudo ln -s /bin/python2.7 /usr/bin/python
 sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache python2 python2-dev python-pipzip python2.7 unzip lld llvm clang clang-tools tar gzip bzip2 rar unrar cpio jq llvm
 echo "Installing packages has been done"
