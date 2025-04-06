060d2275236b574f4a22734b7046992f8b2acfbb
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-06 18:53:32 -0400

    Update packages.sh

--- tools/build/packages.sh
+++ tools/build/packages.sh
@@ -1,16 +1,7 @@
 #!bin/bash
 
 sudo apt-get update
-sudo apt update 
-sudo apt install ccache
-sudo apt install python2
-sudo apt install python2-dev
-sudo apt-get install python2
-sudo apt-get install python2-dev
-sudo apt-get install lld
-sudo apt-get install llvm
-sudo apt-get install clang
-sudo apt-get install clang-tools
+sudo apt update
 sudo apt-get install -y ccache
-sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache zip unzip tar gzip bzip2 rar unrar cpio jq llvm
+sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache python2 python2-dev zip unzip lld llvm clang clang-tools tar gzip bzip2 rar unrar cpio jq llvm
 echo "Installing packages has been done"
