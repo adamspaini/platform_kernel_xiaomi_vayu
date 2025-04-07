2cbe59101525add5f5dcd75669ba9d21510461c9
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-06 21:56:19 -0400

    Update packages.sh

--- tools/build/packages.sh
+++ tools/build/packages.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 
 sudo apt-get update
-sudo apt update 
+sudo apt update
+sudo apt install device-tree-compiler
 sudo apt install ccache
 sudo apt install python2
 sudo apt-get install python2
