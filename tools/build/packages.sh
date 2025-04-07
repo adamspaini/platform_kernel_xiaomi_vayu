b61c8fdd0d4736a11c2352f6910853a626163767
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-06 21:06:22 -0400

    Update packages.sh

--- tools/build/packages.sh
+++ tools/build/packages.sh
@@ -1,10 +1,14 @@
 #!bin/bash
 
 sudo apt-get update
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
+sudo apt update 
+sudo apt install ccache
 sudo apt install python2
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev \
-  libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache python2 python2-dev zip unzip tar gzip bzip2 rar unrar cpio jq llvm wget zstd
+sudo apt-get install python2
+sudo apt-get install lld
+sudo apt-get install llvm
+sudo apt-get install clang
+sudo apt-get install clang-tools
+sudo apt-get install -y ccache
+sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache python2 python2-dev zip unzip tar gzip bzip2 rar unrar cpio jq llvm
 echo "Installing packages has been done"
