8cb8ca4874458bff580dffa8c2f720863ebe1960
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-06 21:53:35 -0400

    Update packages.sh

--- tools/build/packages.sh
+++ tools/build/packages.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
 sudo apt-get update
+sudo apt update
+sudo apt install device-tree-compiler
 sudo apt-get install -y build-essential bc lld gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi libssl-dev libfl-dev curl git ftp lftp wget libarchive-tools ccache python2 python2-dev zip unzip tar gzip bzip2 rar unrar cpio jq llvm
 echo "Installing packages has been done"
